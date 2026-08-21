b22e11a6e9f6b7baff66303d7f41a1edf0802dad
Lazaro Ezequiel Martinez <lazamartinez1999@gmail.com>
2026-08-21 05:16:44 -0300

    refactor(ui): eliminacion de etiquetas de Casos de Uso y correccion de consulta SQL en UnidadRepository

--- src/main/java/com/app/idoneos/repository/modulo_gestion_academica/UnidadRepository.java
+++ src/main/java/com/app/idoneos/repository/modulo_gestion_academica/UnidadRepository.java
@@ -1,8 +1,6 @@
 package com.app.idoneos.repository.modulo_gestion_academica;
-import com.app.idoneos.service.Reportes.*;
 
 import com.app.idoneos.model.*;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,10 +11,9 @@ import java.util.List;
 @Repository
 public interface UnidadRepository extends JpaRepository<Unidad, Integer> {
 
-    @Query("SELECT DISTINCT c.unidad FROM Cronograma c WHERE c.programa.curso = :curso AND c.unidad.baja = false ORDER BY c.numeroOrden ASC")
+    @Query("SELECT c.unidad FROM Cronograma c WHERE c.programa.curso = :curso AND c.unidad.baja = false ORDER BY c.numeroOrden ASC")
     List<Unidad> findByCursoAndBajaFalseOrderByNumeroOrdenAsc(@Param("curso") Curso curso);
 
-    @Query("SELECT DISTINCT c.unidad FROM Cronograma c WHERE c.programa = :programa AND c.unidad.baja = false ORDER BY c.numeroOrden ASC")
+    @Query("SELECT c.unidad FROM Cronograma c WHERE c.programa = :programa AND c.unidad.baja = false ORDER BY c.numeroOrden ASC")
     List<Unidad> findByPrograma(@Param("programa") com.app.idoneos.model.Programa programa);
 }
-
